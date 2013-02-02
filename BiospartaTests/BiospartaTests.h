dfdb265ebe830ac5861f74ef07f9af52983aac39
Jesus Cagide <set311@MacBook-Air-de-Jesus.local>
2013-02-01 20:22:06 -0600

    Initial Commit

---
+++ BiospartaTests/BiospartaTests.h
@@ -0,0 +1,13 @@
+//
+//  BiospartaTests.h
+//  BiospartaTests
+//
+//  Created by Jesus Cagide on 01/02/13.
+//  Copyright (c) 2013 Jesus Cagide. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BiospartaTests : SenTestCase
+
+@end
---
+++ BiospartaTests/BiospartaTests.m
@@ -0,0 +1,32 @@
+//
+//  BiospartaTests.m
+//  BiospartaTests
+//
+//  Created by Jesus Cagide on 01/02/13.
+//  Copyright (c) 2013 Jesus Cagide. All rights reserved.
+//
+
+#import "BiospartaTests.h"
+
+@implementation BiospartaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BiospartaTests");
+}
+
+@end
